50511b7b3bc59b2b0090af4d7561689e0d1c9f91
Terrence Nixa <tnixa@us.ibm.com>
2020-01-31 10:19:10 -0600

    mac openshift tests

---
+++ mac/sandbox/integration_helper.bash
@@ -0,0 +1,72 @@
+#!/bin/bash
+# function to setup the test environment
+function setupenv() {
+    # write the date in to the file
+    DATE=$(date +%Y%m%d_%H%M%S)
+    mkdir -p $HOME/bats_temp/logs/$DATE
+    echo $DATE > $DATEFILE
+
+    # clone the stacks and appsody repos
+    # build the appsody binary
+    mkdir -p $HOME/bats_temp/temp/src/github.com/appsody
+    export GOPATH=$HOME/bats_temp/temp
+    cd $HOME/bats_temp/temp/src/github.com/appsody
+    #echo "#### Cloning stacks repo ####"
+    git clone --quiet https://github.com/appsody/stacks.git
+    cd stacks
+    git rev-parse HEAD > $HOME/bats_temp/logs/$DATE/git-commit-hash-stacks.log
+    cd - > /dev/null
+    #echo "#### Cloning appsody repo ####"
+    git clone --quiet https://github.com/appsody/appsody.git
+    cd appsody
+    git rev-parse HEAD > $HOME/bats_temp/logs/$DATE/git-commit-hash-appsody.log
+    #echo "#### Building appsody binary ####"
+    make build-darwin > /dev/null
+    # make build-linux > /dev/null
+    # make build-windows
+}
+
+# variables for the log datafile and the temp setup directory
+DATEFILE=$HOME/bats_temp/logs/datefile
+SETUPDIR=$HOME/bats_temp/temp/
+
+# if check for the log datefile
+# if the file exists set the date to the first line of the file
+# else run setupenv
+if [ -f "$DATEFILE" ]; then
+    DATE=$(head -n 1 $DATEFILE)
+else
+    setupenv
+fi
+
+# if check for setup directory
+# if is doesn't exist run setupenv
+if [ ! -d "$SETUPDIR" ]; then
+    setupenv
+fi
+
+# exports for use with the tests
+export DATEFILE
+export GOPATH=$HOME/bats_temp/temp
+export LOGDIR=$HOME/bats_temp/logs/$DATE
+export TEMPDIR=$HOME/bats_temp/temp/src/github.com/appsody
+export APPSODY=$TEMPDIR/appsody/build/appsody-0.0.0-darwin-amd64 
+# export APPSODY=$TEMPDIR/appsody/build/appsody-0.0.0-linux-amd64
+#export APPSODY=$TEMPDIR/appsody/build/appsody-0.0.0-windows-amd64.exe
+
+# set APPSODY to appsody if testing the locally built binary (release test)
+# export APPSODY=appsody
+
+export TAG=appsody-mac/express
+export PULLURL=image-registry.openshift-image-registry.svc:5000
+export PUSHURL=default-route-openshift-image-registry.apps.appsodyopenshift42.os.fyre.ibm.com
+export NAMESPACE=appsody-mac
+
+# for fyre openshift tests you need to log into oc and docker...
+# log into the openshift ui as kubeadmin and then in the top right corner click the kube:admin and then
+# Copy Login command and then run the oc login command provided
+# docker login -u admin -p $(oc whoami -t) default-route-openshift-image-registry.apps.appsodyopenshift42.os.fyre.ibm.com
+
+# for crc openshift tests you need to log into oc and docker...
+# oc login -u developer -p developer https://api.crc.testing:6443
+# docker login -u $(oc whoami) -p $(oc whoami -t) https://default-route-openshift-image-registry.apps-crc.testing
